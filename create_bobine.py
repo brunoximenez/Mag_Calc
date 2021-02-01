83d140d7742013a7fe43bb34cd9187e9495689b3
Ximenez Rodrigues Alves Bruno <bruno.ximenez@obspm.fr>
2021-02-01 09:16:16 +0100

    Wrote initial report on the solenoid design for PROCURE. Included tilted solenoid study.

--- create_bobine.py
+++ create_bobine.py
@@ -4,47 +4,70 @@ from magpylib.source.magnet import Box, Cylinder
 from magpylib.source.current import Circular
 from magpylib import Collection, displaySystem
 
+fontsize = 18
 
-def bobine(current, diameter, n_coils, distance):
 
-	distance = diameter 
-	dx_coils = 0.3
+def bobine(current, diameter, n_coils, distance, rotate=False):
 
-	coil_setup = Collection()
-	for j in range(n_coils):
-		z = j * dx_coils + distance / 2
-		coil_p = Circular(curr=current, dim=diameter, pos=(0, 0, z))
-		coil_m = Circular(curr=current, dim=diameter, pos=(0, 0, - z))
-		coil_setup.addSources(coil_p, coil_m)
+    dx_coils = 4.
 
-	return coil_setup
+    coil_setup = Collection()
+    for j in range(n_coils):
+        z = j * dx_coils + distance / 2
+        coil_p = Circular(curr=current, dim=diameter, pos=([0, 0, z]))
+        if rotate == True: coil_p.rotate(10, [1, 0, 0], anchor=[0, 0, 0])
+        coil_m = Circular(curr=current, dim=diameter, pos=([0, 0, - z]))
+        coil_setup.addSources(coil_p, coil_m)
+
+    return coil_setup
 
 
 def calc_mod(b):
-	b_mod = 0
-	for i in range(3):
-		b_mod += b[i] ** 2
+    b_mod = 0
+    for i in range(3):
+        b_mod += b[i] ** 2
 
-	return np.sqrt(b_mod)
+    return np.sqrt(b_mod)
 
 
+def B_array(coll, distance):
+    # Distances in mm
+    dx = 0.5
+    n_points = 100
+    z = np.array([])
+    b = np.array([])
+    label = 'Distance between coils: ' + str(distance) + ' mm'
+    for j in range(1, n_points + 1):
+        x = j * dx / n_points
+        b_calc = coll.getB([0, 0, x])
+        print(b_calc)
+        b = np.append(b, calc_mod(b_calc))
+        z = np.append(z, x)
+        b_calc = coll.getB([0, 0, - x])
+        b = np.append(b, calc_mod(b_calc))
+        z = np.append(z, - x)
+    plt.figure('Magnetic Field')
+    plt.plot(z, b, 'o', label=label)
+    plt.ylabel('B (mT)', fontsize=fontsize)
+    plt.xlabel('z (mm)', fontsize=fontsize)
+    plt.legend(fontsize=14)
+    # plt.show()
 
-def B_array(coll):
-	# Distances in mm
-	dx = 0.2
-	n_points = 100
-	z = np.array([])
-	b = np.array([])
-	for j in range(1, n_points + 1):
-		x = j * dx / n_points
-		b_calc = coll.getB([0,0,x])
-		print(b_calc)
-		b = np.append(b, calc_mod(b_calc))
-		z = np.append(z, x)
-		b_calc = coll.getB([0,0, - x])
-		b = np.append(b, - calc_mod(b_calc))
-		z = np.append(z, - x)
-	plt.plot(z, b, 'o')
-	print(b)
-	# plt.show()
 
+def heat_dissipated():
+	#ohm mm2 /m
+	rho = 0.0171
+	N = 4
+	r_coil = 40e-3 # in m
+	r_wire = 1.5 # in mm
+	I = 20 # Amps
+
+	L = N * 2 * 3.14 * r_coil
+	r_sol = 2 * r_coil * N * rho / (r_wire ** 2)
+	print('Resistance solenoid:')
+	print(r_sol)
+	p_dissipated = r_sol * I ** 2
+	print('Power dissipated:')
+	print(p_dissipated)
+	print('Total lenght of sol:')
+	print(L)
--- solenoid.py
+++ solenoid.py
@@ -6,45 +6,66 @@ from magpylib import Collection, displaySystem
 import create_bobine as bob
 import sys
 
+bob.heat_dissipated()
+sys.exit(0)
+'''
+Some plot settings
+'''
+plt.rc('xtick', labelsize=14)
+plt.rc('ytick', labelsize=14)
+
+# TeX settings
+plt.rcParams['text.usetex'] = True
+plt.rcParams['text.latex.preamble'] = [
+    r'\usepackage{amsmath}']  # for \text command
+
+
 # Create coils
 # Dimension is the diameter in mm
 # Current in amps
-diameter = 8.
+radius = 40.
 current = 20.
-n_coils =  5
+n_coils = 6
+distance = 2 * radius
 
-coll = bob.bobine(current, diameter, n_coils, diameter)
+for i in range(1):
+    dl = i * 2.
+    coll = bob.bobine(current, 2 * radius, n_coils, distance, rotate=False)
+    bob.B_array(coll, distance + dl)
 
-bob.B_array(coll)
+
+
 # sys.exit()
 # calculate B-field on a grid
-ptx = 33
-ptz = 44
-xs = np.linspace(-10, 10, ptx)
-zs = np.linspace(-10, 10, ptz)
+ptx = 100
+ptz = 100
+xs = np.linspace(-100, 100, ptx)
+zs = np.linspace(-100, 100, ptz)
 POS = np.array([(x, 0, z) for z in zs for x in xs])
 
 Bs = coll.getB(POS).reshape(ptz, ptx, 3)  # <--automatically vectorized
 
-B_center = coll.getB([0,0,0])
-print(B_center) 
-
+B_center = coll.getB([0, 0, 0])
+plt.show()
+# sys.exit(0)
 # create figure
 fig = plt.figure(figsize=(9, 5))
 ax1 = fig.add_subplot(121, projection='3d')  # 3D-axis
 ax2 = fig.add_subplot(122)  # 2D-axis
+
 
 # display system geometry on ax1
 displaySystem(coll, subplotAx=ax1, suppress=True)
 # display field in xz-plane using matplotlib
 X, Z = np.meshgrid(xs, zs)
 
-# Take components Bx and Bz 
+
+# Take components Bx and Bz
 Bx, By, Bz = Bs[:, :, 0], Bs[:, :, 1], Bs[:, :, 2]
 
 
-
 # ax2.streamplot(X, Z, U, V, color=np.log(U**2 + V**2))
-b_map = ax2.streamplot(X, Z, Bx, Bz, color=np.sqrt(Bx**2 + By**2 + Bz**2), cmap='autumn')
+b_map = ax2.streamplot(X, Z, Bx, Bz, color=np.sqrt(
+    Bx**2 + By**2 + Bz**2), cmap='autumn')
 fig.colorbar(b_map.lines)
 plt.show()
